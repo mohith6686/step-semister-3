df392f264803905f5416293f26be72ba117cede8
mohith6686 <mr5285@srmist.edu.in>
2026-09-12 11:48:28 +0530

    Add Week 3 Assignment Problems

---
+++ Assignments/EmployeeStatic.java
@@ -0,0 +1,40 @@
+public class EmployeeStatic {
+
+    String empName;
+    double salary;
+
+    static String companyName =
+        "Bright Horizon Technologies";
+
+    static int employeeCount = 0;
+
+    EmployeeStatic(String empName, double salary) {
+
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+
+        System.out.println(companyName);
+        System.out.println(
+            "Employees on record: " + employeeCount
+        );
+    }
+
+    public static void main(String[] args) {
+
+        EmployeeStatic employee1 =
+            new EmployeeStatic("Ravi", 50000);
+
+        EmployeeStatic employee2 =
+            new EmployeeStatic("Priya", 60000);
+
+        EmployeeStatic employee3 =
+            new EmployeeStatic("Arjun", 55000);
+
+        EmployeeStatic.printCompanyInfo();
+    }
+}
